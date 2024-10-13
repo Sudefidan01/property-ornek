4707ad86164c055d2346555aabaf2bf82e186d37
sude <sudefidan@icloud.com>
2024-10-13 19:08:01 +0300

    property-ornek

---
+++ 20230819_Property_Ornek/20230819_Property_Ornek/Kullanici.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230819_Property_Ornek
+{
+    internal class Kullanici
+    {
+        public string kullaniciAdi;
+        private string _parola;
+
+        public string ParolaTanimla
+        {
+            set
+            {
+                _parola = value;
+            }
+        }
+
+        public void GirisYap(string kAdi,string sifre)
+        {
+            if (kAdi==kullaniciAdi && sifre==_parola)
+            {
+                Console.WriteLine("Giriş Yapıldı");
+            }
+            else
+            {
+                Console.WriteLine("Kullanıcı Adı veya Şifre Hatalı");
+            }
+        }
+    }
+}
---
+++ 20230819_Property_Ornek/20230819_Property_Ornek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230819_Property_Ornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kullanici k= new Kullanici();
+            k.kullaniciAdi = "admin";
+            k.ParolaTanimla = "123";
+
+            k.GirisYap("admin","123");
+
+            Console.ReadKey();
+        }
+    }
+}
